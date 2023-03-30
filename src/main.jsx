9b5aecca3132380286ddeeea5c01713c504a1547
Alonso9 <ramonalonso_giron@ucol.mx>
2023-03-30 17:02:26 -0600

    Erro de vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GifExpertApp } from './GirExpertApp/GifExpertApp'
-import './GirExpertApp/GifExpertstyle.css'
+import "./GirExpertApp/GifExpertstyle.css"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GifExpertApp/>
